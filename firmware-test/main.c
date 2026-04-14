e350ae79267c7d7bb21c9f3a6fd1bbf470250ac0
pradyumnajalan <pradumna79@gmail.com>
2026-04-14 12:48:31 +0530

    Fix main.c - remove dereferencing of null ptr

--- firmware-test/main.c
+++ firmware-test/main.c
@@ -6,8 +6,6 @@ void delay(uint32_t count) {
 
 int main(void) {
     volatile uint32_t *led = (uint32_t *)0x40020C18;
-    int* p = NULL;
-    *p = 42;
     while(1) {
         *led ^= (1 << 13);
         delay(1000000);
