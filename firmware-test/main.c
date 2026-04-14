f486d662486292ecd1200658c47fd4c93adb1b7b
pradyumnajalan <pradumna79@gmail.com>
2026-04-14 13:13:56 +0530

    Minor change in main.c

--- firmware-test/main.c
+++ firmware-test/main.c
@@ -7,7 +7,7 @@ void delay(uint32_t count) {
 int main(void) {
     volatile uint32_t *led = (uint32_t *)0x40020C18;
     while(1) {
-        *led ^= (1 << 13);
+        *led ^= (1 << 12);
         delay(1000000);
     }
     return 0;
